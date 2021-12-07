dc1c90af02ab5515740ba9dc89f6bf4f98a90355
rissba05 <rissba05@gmail.com>
2021-12-07 11:15:00 -0600

    got some client side routing working..
    remember routerLink Angular attribute instead of href

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { KittyComponent } from './kitty/kitty.component';
+import { WorldComponent } from './world/world.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: ''
+    , component: WorldComponent
+  }
+  , {
+    path: 'world'
+    , component: WorldComponent
+  }
+  , {
+    path: 'kitty'
+    , component: KittyComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
